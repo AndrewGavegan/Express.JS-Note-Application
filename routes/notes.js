2d82d6b7fa0ec1b61c85827ec9f13883d339adc9
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-10 19:38:47 +1000

    fixing placement of router code

--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,25 @@
 // requiring the helper functions that the fs package provides us with, in this route so we can set up the functionality //
 const { readFromFile, readAndAppend } = require('../heplers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
-
+const express = require('express');
+const notes = express.Router();
 // passed in an app function requirement in the server.js file so now we pass that arguement into this router function //
 
-module.exports = function (app) {
-    app.get('/', (req, res) => {
+
+    notes.get('/', (req, res) => {
         readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
     });
 
-    app.post('/', (req, res) => {
+    notes.post('/', (req, res) => {
         console.log(req.body);
 
-        const {Note} = req.body;
+        const {title, text} = req.body;
+ // using unique id package to give each note its own identifier //
+        const Note = {title, text, id: uuidv4() };
 
-            // using unique id package to give each note its own identifier //
-        Note.id = uuid.v4();
-            // getting the data that got put in database before //
-        let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-        // pushed data from json file database into the req.body //
-        data.push(Note) // stringifying new note //
-        fs.writeFileSync('./db/db.json', JSON.stringify(data));
-        res.json(data);
-
+           
+       readAndAppend(Note, './db/db.json');
+       res.json('Note Added')
     })
-}
+
+    module.exports = notes;
--- server.js
+++ server.js
@@ -10,11 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-// notes and index routes //
-require('./routes/notes')(app);
-require('./routes/index')(app);
+   // notes page route //
+   app.get('/notes', (req, res) =>
+   res.sendFile(path.join(__dirname, '../public/notes.html'))
+   );
 
-
+   // homepage route //
+   app.get('*', (req, res) => 
+   res.sendFile(path.join(__dirname, '../public/index.html'))
+   );
+   
 // link port to app //
 app.listen(PORT, () =>
     console.log(`Note App listening at http://localhost:${PORT} 🚀`)
